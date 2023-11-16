572fc212f34d9d1666ba098ca5ad8a97ceb95632
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-11-16 21:04:59 +0100

    feat: update docs to 0.3.11 (#9)

--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['try_5fkls_5fgulp_5ffile_0',['try_kls_gulp_file',['../koliseo_8c.html#a541127eb786413201b4fcaeb7f406e78',1,'koliseo.c']]]
+  ['try_5fkls_5fgulp_5ffile_0',['try_kls_gulp_file',['../koliseo_8c.html#a541127eb786413201b4fcaeb7f406e78',1,'koliseo.c']]],
+  ['try_5fkls_5fgulp_5ffile_5fto_5fkstr_1',['try_kls_gulp_file_to_kstr',['../koliseo_8c.html#a514ca7b57a5ccb88a62559e8dedee184',1,'koliseo.c']]]
 ];
